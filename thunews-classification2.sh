0a25f4fcdcec60e07b00aa6b3f7ea4aa9010b383
zhezhaoxu <zhezhaoxu@tencent.com>
2018-06-11 11:32:50 +0800

    update

--- thunews-classification2.sh
+++ thunews-classification2.sh
@@ -14,7 +14,7 @@ DATADIR=data/THUCNews
 mkdir -p "${RESULTDIR}"
 mkdir -p "${DATADIR}"
 
-./fasttext supervised -input "${DATADIR}/thucnews.2.train" -output "${RESULTDIR}/thucnews-c.2" -dim 50 -lr 0.1 -wordNgrams 3 -minCount 1 -bucket 10000000 -epoch 20 -thread 8
+./fasttext supervised -input "${DATADIR}/thucnews.2.train" -output "${RESULTDIR}/thucnews-c.2" -dim 100 -lr 0.1 -ws 5 -neg 5 -loss ns -wordNgrams 2 -minCount 5 -bucket 10000000 -t 1e-4 -lrUpdateRate 100 -epoch 10 -thread 8
 
 #./fasttext test "${RESULTDIR}/thucnews-c.bin" "${DATADIR}/thucnews.test"
 
